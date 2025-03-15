847d0181fe1b99e3f873c1907851b07082599c3b
richardimaoka <richard.s.imaoka@gmail.com>
2025-03-15 18:29:11 +0900

    console.log

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import { schema } from "prosemirror-schema-basic";
 import { addListNodes } from "prosemirror-schema-list";
 import { exampleSetup } from "prosemirror-example-setup";
 
+console.log("schema.spec.nodes", JSON.stringify(schema.spec.nodes));
+console.log(
+  "addListNodes(...)",
+  JSON.stringify(addListNodes(schema.spec.nodes, "paragraph block*", "block"))
+);
+
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
 const mySchema = new Schema({
