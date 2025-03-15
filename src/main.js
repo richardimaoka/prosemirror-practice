d9df95a2d82902c62f3985e62f81121c089ce764
richardimaoka <richard.s.imaoka@gmail.com>
2025-03-15 15:01:06 +0900

    basic example

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
-import { Schema } from "prosemirror-model";
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
+import { Schema, DOMParser } from "prosemirror-model";
+import { schema } from "prosemirror-schema-basic";
+import { addListNodes } from "prosemirror-schema-list";
+import { exampleSetup } from "prosemirror-example-setup";
 
-const schema = new Schema({
-  nodes: {
-    doc: {
-      content: "paragraph+",
-    },
-    paragraph: {
-      content: "text*",
-      toDOM() {
-        return ["h1", 0];
-      },
-    },
-    text: {},
-  },
+// Mix the nodes from prosemirror-schema-list into the basic schema to
+// create a schema with list support.
+const mySchema = new Schema({
+  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+  marks: schema.spec.marks,
 });
-const state = EditorState.create({ schema });
-new EditorView(document.getElementById("app"), { state });
+
+window.view = new EditorView(document.querySelector("#app"), {
+  state: EditorState.create({
+    doc: DOMParser.fromSchema(mySchema).parse(
+      document.querySelector("#content")
+    ),
+    plugins: exampleSetup({ schema: mySchema }),
+  }),
+});
